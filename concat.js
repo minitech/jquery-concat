a2e9833509534095b937c5a35633e4c75b0818e3
Peter Olson <peterofthecorn@gmail.com>
2013-02-14 21:48:52 -0600

    Removing null characters from string to fix issue #2

--- concat.js
+++ concat.js
@@ -1,8 +1,15 @@
 (function() {
 	'use strict';
 
+	function removeNulls(strings) {
+	    for (var i = 0; i < strings.length; i++) {
+	        strings[i] = strings[i].toString().replace(/\0/g, "");
+	    }
+	    return strings;
+	}
+
 	function concat() {
-		return Array.prototype.join.call(arguments, '');
+		return Array.prototype.join.call(removeNulls(arguments), '');
 	}
 
 	concat.load = function(jQuery) {
